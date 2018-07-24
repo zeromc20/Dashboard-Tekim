22604961c5805bbf12233799fe7047274b2dbe45
fadhilah@mhs.unsyiah.ac.id <123_akmal>
2018-07-24 20:30:47 +0700

    kp

--- resources/views/mhs/kerjapraktek/daftar.blade.php
+++ resources/views/mhs/kerjapraktek/daftar.blade.php
@@ -26,39 +26,43 @@
                             <div class="form-group row">
                                 <label for="nama_perusahaan" class="col-sm-4 col-form-label-lg text-md-right" >{{__('Nama Perusahaan')}}</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" id="nama_perusahaan" name="nama_perusahaan" value="{{ old('nama_perusahaan') }}" required autofocus>
+                                    <input type="text" class="form-control" id="nama_perusahaan" name="nama_perusahaan" value="{{ old('nama_perusahaan') }}"  autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="alamat_perusahaan" class="col-sm-4 col-form-label-lg text-md-right">{{__('Alamat Perusahaan')}}</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" id="alamat_perusahaan" name="alamat_perusahaan" value="{{old('alamat_perusahaan')}}" required autofocus>
+                                    <input type="text" class="form-control" id="alamat_perusahaan" name="alamat_perusahaan" value="{{old('alamat_perusahaan')}}"  autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="nomor_surat" class="col-sm-4 col-form-label-lg text-md-right">{{__('Nomor Surat')}}</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" id="nomor_surat" name="nomor_surat" value="{{old('nomor_surat')}}" required autofocus>
+                                    <input type="text" class="form-control" id="nomor_surat" name="nomor_surat" value="{{old('nomor_surat')}}"  autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="periode" class="col-sm-4 col-form-label-lg text-md-right">{{__('Periode ')}}</label>
-                                <div>
-                                    <input type="date" class="form-control col-md-1" id="periode" name="periode" value="{{old('periode')}}" required autofocus>
-                                    <span class="col-form-label-lg">Sampai &ensp;</span>
-                                    <input type="date" class="form-control col-md-1" id="akhir_periode" value="{{old('akhir_periode')}}">
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control" id ="periode" name="periode" value="{{old('periode')}}" autofocus >
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="akhir_periode" class="col-sm-4 col-form-label-lg text-md-right">{{__('Akhir Periode')}}</label>
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control" id="akhir_periode" name="akhir_periode" value="{{old('akhir_periode')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="ipk" class="col-sm-4 col-form-label-lg text-md-right">{{__('IPK')}}</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" id="ipk" name="ipk" value="{{old('ipk')}}" required autofocus>
+                                    <input type="text" class="form-control" id="ipk" name="ipk" value="{{old('ipk')}}"  autofocus>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="jumlah_sks" class="col-sm-4 col-form-label-lg text-md-right">{{__('Jumlah SKS')}}</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" id="jumlah_sks" name="jumlah_sks" value="{{old('jumlah_sks')}}" required autofocus>
+                                    <input type="text" class="form-control" id="jumlah_sks" name="jumlah_sks" value="{{old('jumlah_sks')}}" autofocus>
                                 </div>
                             </div>
                         </form>
